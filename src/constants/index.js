51ac52e5d41033f8784981fc620cdf6b253d9da7
Dann Wee <dannweezj@gmail.com>
2023-05-13 12:12:46 +0800

    3d portfolio website codes

---
+++ src/constants/index.js
@@ -0,0 +1,277 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    tailwind,
+    nodejs,
+    mongodb,
+    figma,
+    solidity,
+    python,
+    cprogram,
+    cplusplus,
+    threejs,
+    ey,
+    elastos,
+    saf,
+    adactiveasia,
+    youview,
+    simpleregistry,
+    mobilma,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Software Developer",
+      icon: web,
+    },
+    {
+      title: "Tech Enthusiast",
+      icon: creator,
+    },
+    {
+      title: "Web3 Evangelist",
+      icon: backend,
+    },
+    {
+      title: "Conceptual Thinker",
+      icon: mobile,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "Python",
+      icon: python,
+    },
+    {
+      name: "CProgramming",
+      icon: cprogram,
+    },
+    {
+      name: "C++",
+      icon: cplusplus,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "solidity",
+      icon: solidity,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "IT Development Intern",
+      company_name: "Ernst & Young | FinTech ASEAN",
+      icon: ey,
+      iconBg: "#383E56",
+      date: "March 2018 - August 2018",
+      points: [
+        "Establish EY's FinTech capabilities across ASEAN and strengthening the FinTech ecosystem in the region.",
+        "Assisted in organising and collaborating with Monetary Authority Singapore (MAS) for the Singapore FinTech Festival – Investor Summit 2018.",
+        "Generated marketing collaterals such as Comics, Videos, Infographics for investor & start-up relations.",
+        "Assisted in building several essential investor-start-up project decks for team to propose to clients.",
+        "Prepared a Proof-of-Concept Kit consisting of more than 20 PoC solutions.",
+        "Facilitated in conceptualising and launching of the EY FinTech Hub, a digital marketplace for financial institutions to discover and procure solutions in domains of RegTech, Blockchain, Lending, Artificial Intelligence, Payments, Cyber Security, WealthTech, InsurTech and Open Banking.",
+
+      ],
+    },
+    {
+      title: "Blockchain Tech Evangelist",
+      company_name: "Elastos | Web3",
+      icon: elastos,
+      iconBg: "#E6DEDD",
+      date: "August 2018 - October 2018",
+      points: [
+        "Co-organiser of an Elastos event held in Singapore with over 80 participants.",
+        "Promoted uses of the Blockchain Technology to 50 software developers and blockchain enthusiasts.",
+        "Developed a communication channel with 45 members to disseminate information and announcements.",
+        "Raised awareness for Elastos Blockchain system to encourage software developers to code dApps on the Elastos Blockchain Ecosystem.",
+      ],
+    },
+    {
+      title: "Platoon Instructor",
+      company_name: "Singapore Armed Forces (SAF)",
+      icon: saf,
+      iconBg: "#383E56",
+      date: "September 2020 - August 2021",
+      points: [
+        "Planned and facilitated over 20 physically intensive trainings with proper safety management.",
+        "Administered meaningful, effective, and safe trainings for 300 trainees.",
+        "Supervised platoon’s welfare, training, and operations of over 100 personnel in total.",
+        "Regulated the Covid-19 operational plan of over 300 personnel in total.",
+        "Participated in National Day Parade 2021 as the SAFTI Ensign, Colours Party.",
+      ],
+    },
+    {
+      title: "Growth Markets Intern",
+      company_name: "Adactive Asia | Web3",
+      icon: adactiveasia,
+      iconBg: "#E6DEDD",
+      date: "May 2022 - August 2022",
+      points: [
+        "Establish Adactive Asia's Web3 network-building application called Wanderverse",
+        "Designed several Web3 marketing collaterals for Facebook, Instagram, Twitter and Discord.",
+        "Refined multiple essential investor decks and marketing decks to attract more partnerships.",
+        "Conducted intensive research in Web3 Development, Smart Contracts and Tokenomics.",
+        "Outsourced to over 200 merchants and charity organisations for partnerships.",
+        "Liaised with Polygon Blockchain’s Marketing Team to develop marketing strategies for the Web3 Mobile Application.",
+        "Constructed an informative website using HTML and CSS",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "YouView",
+      description:
+        "A tool for YouTube content creators to optimise their videos and grow their channels based on views.",
+      tags: [
+        {
+          name: "JupyterNotebook",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Python",
+          color: "green-text-gradient",
+        },
+        {
+          name: "DataAnalysis",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: youview,
+      source_code_link: "https://github.com/dannweeeee/SC1015_YouView",
+    },
+    {
+      name: "Moblima",
+      description:
+        "A 3-in-1 online movie console application called MOvie Booking and LIsting Management Application (MOBLIMA).",
+      tags: [
+        {
+          name: "Java",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "JavaScript",
+          color: "green-text-gradient",
+        },
+        {
+          name: "JavaDoc",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: mobilma,
+      source_code_link: "https://github.com/dannweeeee/SC2002_MOBLIMA",
+    },
+    {
+      name: "Simple Registry",
+      description:
+        "A Simple Name Registry created using Solidity with State Inheritance Testing Approach",
+      tags: [
+        {
+          name: "Solidity",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Blockchain",
+          color: "green-text-gradient",
+        },
+        {
+          name: "SmartContracts",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: simpleregistry,
+      source_code_link: "https://github.com/dannweeeee/SimpleRegistry",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
